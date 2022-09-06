e8449bda6d488d4095a1039788a8547fb983f5b3
roller <roller0516@naver.com>
2022-09-07 02:12:12 +0900

    시네마실
    시네마신

--- unity/Assets/code/playermove.cs
+++ unity/Assets/code/playermove.cs
@@ -6,6 +6,8 @@ public class playermove : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody characterRigidbody;
+    
+    [SerializeField] private SpriteRenderer _spriteRenderer;
 
     void Start()
     {
@@ -17,10 +19,24 @@ public class playermove : MonoBehaviour
         float inputX = Input.GetAxis("Horizontal");
         float inputZ = Input.GetAxis("Vertical");
         // -1 ~ 1
-
         Vector3 velocity = new Vector3(inputX, 0, inputZ);
         velocity *= speed;
         characterRigidbody.velocity = velocity;
+        
+        ChangeFlipX(inputX);
+    }
+
+
+    void ChangeFlipX(float x)
+    {
+        if (x < 0)//왼쪽
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (x > 0)//오른쪽
+        {
+            _spriteRenderer.flipX = true;
+        }
     }
 }
 
