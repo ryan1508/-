ddb634800d7c2e5fc20134e400bde93c15832964
roller <roller0516@naver.com>
2022-09-17 21:12:36 +0900

    깃 무시

--- unity/Assets/code/Player.cs
+++ unity/Assets/code/Player.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using Project;
+using SKUnityToolkit.SerializableDictionary;
 using UnityEngine;
 
 public class Player : BaseUnit
 {
     public override UnitType BaseUnitType => UnitType.Player;
-
     public float inPutX { get; set; }
-
     public float inPutZ { get; set; }
+    //공격 중인 상태 
+    public bool IsAttack { get; set; }
+    public PlayerData playerData => DataManager.Instance.playerData;
+    
+    [SerializeField] private SerializableDictionary<PlayerAttackType, BoxCollider> attackBoxColl;
     
     public override void Init()
     {
@@ -17,7 +21,7 @@ public class Player : BaseUnit
         unitStates = new Dictionary<PlayerState, IUnitState>();
         
         unitStates.Add(PlayerState.IDLE,new IdleState());
-        unitStates.Add(PlayerState.ATTAK1,new AttackState());
+        unitStates.Add(PlayerState.ATTAK,new AttackState());
         unitStates.Add(PlayerState.RUN,new RunState());
         unitStates.Add(PlayerState.WALK,new IWalkState());
         
@@ -31,7 +35,7 @@ public class Player : BaseUnit
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            ChangeState(PlayerState.ATTAK1);
+            ChangeState(PlayerState.ATTAK);
         }
         
         
@@ -63,5 +67,30 @@ public class Player : BaseUnit
     {
         var unitState = unitStates[playerState];
         ChangeState(unitState);
+    }
+
+    public BoxCollider GetAttackBoxCollider(PlayerAttackType attType)
+    {
+        if (attackBoxColl.ContainsKey(attType) == false)
+        {
+            Debug.LogError($"{attType} 콜라이더 없음.");
+        }
+
+        return attackBoxColl[attType];
+    }
+
+    public void SetBoxCollider(PlayerAttackType attType,bool isEnable)
+    {
+        foreach (var t in attackBoxColl.Keys)
+        {
+            if (t != attType)
+            {
+                attackBoxColl[t].enabled = false;
+            }
+            else
+            {
+                attackBoxColl[t].enabled = isEnable;
+            }
+        }
     }
 }
