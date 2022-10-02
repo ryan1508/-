51fcb97f928b6560e9b54898524793514dee7baf
roller <roller0516@naver.com>
2022-10-02 20:01:04 +0900

    boss

--- unity/Assets/code/Boss.cs
+++ unity/Assets/code/Boss.cs
@@ -10,7 +10,7 @@ public class Boss : MonoBehaviour
     [SerializeField] private Transform effectRoot;
     [SerializeField] private float attackTime;
     [SerializeField] private float pattern1;
-
+    [SerializeField] private Animator Animator;
     private GameObject _gameObject;
     private Material _material;
     [SerializeField]private BossAattackRange Range;
@@ -33,9 +33,14 @@ public class Boss : MonoBehaviour
                 _gameObject.SetActive(false);
                 _gameObject = null;
                 _material = null;
+                JumpAttack(false);
+
+                StartCoroutine(StateReset());
+                
                 return;
             }
-            
+
+            JumpAttack(true);
             if (_gameObject == null)
             {
                 _gameObject = Instantiate(attackRange, effectRoot.transform);
@@ -53,5 +58,27 @@ public class Boss : MonoBehaviour
             if(_material != null)
                 Range.DrawCircle(_material,p1/pattern1); 
         }
+    }
+
+    private void JumpAttack(bool isJump)
+    {
+        if (isJump)
+        {
+            Animator.SetBool("JumpAttack",true);
+            Animator.SetBool("DownAttack",false);
+        }
+        else
+        {
+            Animator.SetBool("JumpAttack",false);
+            Animator.SetBool("DownAttack",true);
+        }
+    }
+
+    private IEnumerator StateReset()
+    {
+        yield return new WaitForSeconds(1f);
+        
+        Animator.SetBool("JumpAttack",false);
+        Animator.SetBool("DownAttack",false);
     }
 }
